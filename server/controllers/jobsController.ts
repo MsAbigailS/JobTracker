68526565cc3397ea9044a6596d1ee48f15d67860
MsAbigailS <msabigailrsmith@gmail.com>
2025-06-05 17:46:55 -0500

    update:gitignore

--- server/controllers/jobsController.ts
+++ server/controllers/jobsController.ts
@@ -1,12 +1,11 @@
-// controllers/jobsController.ts
 import { Request, Response } from 'express';
 import { Job } from '../models/jobs';
 
 export const createJob = async (req: Request, res: Response): Promise<void> => {
     try {
         const { role, company } = req.body;
-        const job = new Job({ role, company }); // ✅ use Mongoose model
-        await job.save(); // ✅ works now
+        const job = new Job({ role, company });
+        await job.save();
         res.status(201).json(job);
     } catch (error) {
         console.error('Error creating job:', error);
